c26445ad7b4cf3818bf3fbae1495418c30e4e865
StaurosMaz <smazanakis698@gmail.com>
2025-11-03 08:10:26 +0200

    added fs

--- node_js/QR-code_generator/app.js
+++ node_js/QR-code_generator/app.js
@@ -1,10 +1,12 @@
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 inquirer
   .prompt([{ message: "type in your URL", name: "URL" }])
   .then((answers) => {
     const url = answers.URL;
     var qr_svg = qr.image(url);
-    qr_svg.pipe(require("fs").createWriteStream("qr_img.svg"));
+    qr_svg.pipe(fs.createWriteStream("qr_img.svg"));
   })
   .catch((error) => {
     if (error.isTtyError) {
