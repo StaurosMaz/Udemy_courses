a99488827c608564ea1693bd7a2653a38f37ea90
StaurosMaz <smazanakis698@gmail.com>
2025-11-03 08:17:55 +0200

    saving users input

--- node_js/QR-code_generator/app.js
+++ node_js/QR-code_generator/app.js
@@ -7,6 +7,9 @@ inquirer
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+      console.log("the file has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
