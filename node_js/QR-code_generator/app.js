5cd992dd3d565284141292a1c917e83ca4dfe6f7
StaurosMaz <smazanakis698@gmail.com>
2025-11-03 08:00:40 +0200

    testing inquirer

--- node_js/QR-code_generator/app.js
+++ node_js/QR-code_generator/app.js
@@ -1,5 +1,15 @@
-const fs = require("fs");
-fs.writeFile("message.txt", "hello node", (err) => {
-  if (err) throw err;
-  console.log("the file has been saved");
-});
+import inquirer from "inquirer";
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
