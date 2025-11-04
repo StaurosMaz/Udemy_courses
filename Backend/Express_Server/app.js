feb5f24c9a3303a2281a391b454c5c453a15e2df
StaurosMaz <smazanakis698@gmail.com>
2025-11-04 11:02:17 +0200

    #

--- Backend/Express_Server/app.js
+++ Backend/Express_Server/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 const app = express();
+app.get("/", (req, res) => {
+  res.send("hello World");
+});
 app.listen(3000, () => {
   console.log("server running on port 3000.");
 });
