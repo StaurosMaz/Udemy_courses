33ce6b9a9558555e17157dd4f13ac4b0b1ff6e63
StaurosMaz <smazanakis698@gmail.com>
2025-11-04 16:38:48 +0200

    testing Endpoints

--- Backend/Express_Server/app.js
+++ Backend/Express_Server/app.js
@@ -3,6 +3,13 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello World");
 });
+app.get("/contact", (req, res) => {
+  res.send("hello contact");
+});
+app.get("/about", (req, res) => {
+  res.send("hello about");
+});
+
 app.listen(3000, () => {
   console.log("server running on port 3000.");
 });
