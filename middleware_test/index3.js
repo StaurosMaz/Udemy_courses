69c50601b55f6a4deb82137ba4060dea0c20cf40
StaurosMaz <smazanakis698@gmail.com>
2025-11-06 08:07:38 +0200

    end  of my custom middlewere that returns reqested url and method

--- middleware_test/index3.js
+++ middleware_test/index3.js
@@ -1,13 +1,11 @@
 import express from "express";
 function logger(req, res, next) {
-  console.log(req.URL, req.method());
+  console.log(req.url, req.method);
   next();
 }
 const app = express();
 const port = 3000;
-
 app.use(logger);
-
 app.get("/", (req, res) => {
   res.send("Hello");
 });
