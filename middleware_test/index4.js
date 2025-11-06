06a669e0b021a9c94e74bf6d2f0ce78f8ec738a9
StaurosMaz <smazanakis698@gmail.com>
2025-11-06 08:13:56 +0200

    making band name generetor useing all the preveious thing that i leanred

--- middleware_test/index4.js
+++ middleware_test/index4.js
@@ -1,5 +1,7 @@
 import express from "express";
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
