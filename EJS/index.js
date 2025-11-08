d85d6612ec1b69c07e9880af5aed4170efca44fe
StaurosMaz <smazanakis698@gmail.com>
2025-11-08 13:53:05 +0200

    #

--- EJS/index.js
+++ EJS/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
 const port = 3000;
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    dayType: "a weekday",
+    advice: "its time to work hard",
+  });
+});
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
