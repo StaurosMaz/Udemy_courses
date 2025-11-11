8d157aa94e2eb8e47c48cf3e0ceb598c8c6b1701
StaurosMaz <smazanakis698@gmail.com>
2025-11-11 10:26:02 +0200

    added the partials and random consts

--- band_generator_project/index.js
+++ band_generator_project/index.js
@@ -4,7 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 3 - Make the styling show up.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
@@ -15,10 +16,13 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.render("index.ejs");
   //Step 1 - Make the get route work and render the index.ejs file.
 });
 
 app.post("/submit", (req, res) => {
+  const radAdj = adj[Math.floor(Math.random() * adj.length)];
+  const radNoun = noun[Math.floor(Math.random() * noun.length)];
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
