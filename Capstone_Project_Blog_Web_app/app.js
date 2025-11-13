e830e45e8332244d41cd7ee11dc4e12406e10534
StaurosMaz <smazanakis698@gmail.com>
2025-11-13 18:31:21 +0200

    completed blog project

--- Capstone_Project_Blog_Web_app/app.js
+++ Capstone_Project_Blog_Web_app/app.js
@@ -2,8 +2,38 @@ import express from "express";
 import bodyParser from "body-parser";
 const app = express();
 
+app.set("view engine", "ejs");
+const posts=[]
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", { posts: posts });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postTitle", (req, res) => {
+  const requestedTitle = req.params.postTitle.toLowerCase();
+
+  posts.forEach(post => {
+    if (post.title.toLowerCase() === requestedTitle) {
+      res.render("post", { title: post.title, content: post.content });
+    }
+  });
+});
+
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
