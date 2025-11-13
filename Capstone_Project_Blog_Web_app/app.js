9048f87341d16221f2950729f60f7a602b33680b
StaurosMaz <smazanakis698@gmail.com>
2025-11-13 07:57:01 +0200

    added express body-parser and ejs

---
+++ Capstone_Project_Blog_Web_app/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
