adfa8e3e33aa855356d0e3f375362d3172db4507
StaurosMaz <smazanakis698@gmail.com>
2026-01-25 13:30:36 +0200

    added all hte files and npm packages

--- personal project python opencv/test2.py
+++ personal project python opencv/test2.py
@@ -11,7 +11,6 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     max_num_hands=1,
     min_detection_confidence=0.80,
-    min_tracking_confidence=0.75
 )
 
 mp_draw = mp.solutions.drawing_utils
