5207274b703a20400a861e948c824a771a4c1d1f
StaurosMaz <smazanakis698@gmail.com>
2026-03-15 16:58:41 +0200

    connected db

--- 8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ 8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -1,5 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "test",
+  password: "12345",
+  port: 5432,
+});
+db.connect();
 
 const app = express();
 const port = 3000;
