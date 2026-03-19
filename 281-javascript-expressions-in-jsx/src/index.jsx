1f676d835c921b37138a4ab396ac8a4bbe67f4c9
StaurosMaz <smazanakis698@gmail.com>
2026-03-19 13:17:26 +0200

    #

--- 281-javascript-expressions-in-jsx/src/index.jsx
+++ 281-javascript-expressions-in-jsx/src/index.jsx
@@ -4,8 +4,14 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const name = "stavros";
-
-root.render(<h1>hello {name}</h1>);
+const num = 4;
+root.render(
+  <div>
+    <h1>hello i am {name}</h1>
+    <p>and my lucky number is {num}</p>
+    <p>and this is a random number {Math.floor(Math.random() * 10)}</p>
+  </div>,
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
