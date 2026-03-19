f829c8d07d742b28dd7377b75c1b563db7d36e3e
StaurosMaz <smazanakis698@gmail.com>
2026-03-19 13:20:44 +0200

    #

--- 281-javascript-expressions-in-jsx/src/index.jsx
+++ 281-javascript-expressions-in-jsx/src/index.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const name = "stavros";
+const fname = "stavros";
+const lname = "maz";
 const num = 4;
 root.render(
   <div>
-    <h1>hello i am {name}</h1>
+    <h1>hello i am {fname + " " + lname}</h1>
     <p>and my lucky number is {num}</p>
     <p>and this is a random number {Math.floor(Math.random() * 10)}</p>
   </div>,
