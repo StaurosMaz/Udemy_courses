bee42d8401b883c1afb92abff8c7e5bcf54def3c
StaurosMaz <smazanakis698@gmail.com>
2026-03-23 12:28:08 +0200

    trying empot export in react

--- 286-react-components/src/index.jsx
+++ 286-react-components/src/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Heading from "./Heading";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <Heading />
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
     </ul>
   </div>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you're running this locally in VS Code use the commands:
