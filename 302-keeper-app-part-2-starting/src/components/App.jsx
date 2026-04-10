59092585cda872f0723dad0329a82ea7e580271e
StaurosMaz <smazanakis698@gmail.com>
2026-04-10 15:29:40 +0300

    added map fun to render the notes

--- 302-keeper-app-part-2-starting/src/components/App.jsx
+++ 302-keeper-app-part-2-starting/src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import notes from "../notes";
+function createNotes(notes) {
+  return (
+    <Note
+      key={notes.key}
+      id={notes.key}
+      title={notes.title}
+      content={notes.content}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
