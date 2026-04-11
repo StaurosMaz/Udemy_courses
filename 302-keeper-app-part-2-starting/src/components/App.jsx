d4c99af36d2ffe7b944b29a2b4fac5c780110053
StaurosMaz <smazanakis698@gmail.com>
2026-04-11 14:28:27 +0300

    code clean up for the map fun and notes

--- 302-keeper-app-part-2-starting/src/components/App.jsx
+++ 302-keeper-app-part-2-starting/src/components/App.jsx
@@ -3,21 +3,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
-function createNotes(notes) {
-  return (
-    <Note
-      key={notes.key}
-      id={notes.key}
-      title={notes.title}
-      content={notes.content}
-    />
-  );
-}
+
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(createNotes)}
+      {notes.map((notes) => (
+        <Note
+          key={notes.key}
+          id={notes.key}
+          title={notes.title}
+          content={notes.content}
+        />
+      ))}
       <Footer />
     </div>
   );
