6dc2d3ba3cb8ab5d57bdd8711eeac5d29b67209d
StaurosMaz <smazanakis698@gmail.com>
2026-04-23 18:03:01 +0300

    trying out event in react

--- 310-event-handling-in-react/src/components/App.jsx
+++ 310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("hello");
+  const [headingStyle, setHeadingStyle] = useState(false);
+  function handleClick() {
+    setHeadingText("all good");
+  }
+
+  function mouseOverHandle() {
+    setHeadingStyle(true);
+    console.log("true");
+  }
+  function mouseOutHandle() {
+    setHeadingStyle(false);
+    console.log("fasle");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: headingStyle ? "black" : "white" }}
+        onMouseOver={mouseOverHandle}
+        onMouseOut={mouseOutHandle}
+        onClick={handleClick}
+      >
+        Submit
+      </button>
     </div>
   );
 }
